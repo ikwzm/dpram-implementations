3e6883fd209f7c5eea81ad32a92f8705846ab80a
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2026-05-02 00:24:01 +0900

    [rename] fpga/ip/axi_dpram -> fpga/ip/axi_dpram_model_0

--- fpga/ip/axi_dpram_model_0/create_ip.tcl
+++ fpga/ip/axi_dpram_model_0/create_ip.tcl
@@ -1,14 +1,14 @@
 #
 # create_ip.tcl  Tcl script for create project and generate IP
 #
-set project_name            "axi_dpram"
-set ip_name                 "AXI_DPRAM"
-set ip_version              "1.0"
+set project_name            "axi_dpram_model_0"
+set ip_name                 "AXI_DPRAM_MODEL_0"
+set ip_version              "1.1"
 set ip_core_revision        1
 set ip_vendor_name          "ikwzm"
 set ip_library_name         "PIPEWORK"
 
-set ip_root_directory       [file join [file dirname [info script]] ".." "axi_dpram_$ip_version"]
+set ip_root_directory       [file join [file dirname [info script]] ".." "axi_dpram_model_0_$ip_version"]
 set project_directory       [file join [file dirname [info script]] "work"]
 set device_parts            "xc7z010clg400-1"
 #
@@ -90,11 +90,11 @@ set_property supported_families {zynq Production virtex7 Production qvirtex7 Pro
 #
 # Set Core Version
 #
-set_property version       "$ip_version"           [ipx::current_core]
-set_property core_revision "$ip_core_revision"     [ipx::current_core]
-set_property name          "$ip_name"              [ipx::current_core]
-set_property display_name  "$ip_name"              [ipx::current_core]
-set_property description   "AXI DPRAM TYPE=MODEL"  [ipx::current_core]
+set_property version       "$ip_version"            [ipx::current_core]
+set_property core_revision "$ip_core_revision"      [ipx::current_core]
+set_property name          "$ip_name"               [ipx::current_core]
+set_property display_name  "$ip_name"               [ipx::current_core]
+set_property description   "AXI DPRAM TYPE=MODEL_0" [ipx::current_core]
 #
 # Associate Clock
 #
